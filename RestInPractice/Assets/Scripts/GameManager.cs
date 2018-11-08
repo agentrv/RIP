2d953bcd6daf57c5d127c702598855ddb1b8ba07
agentrv <agent.vega@gmail.com>
2018-11-07 19:57:46 -0800

    Update

--- RestInPractice/Assets/Scripts/GameManager.cs
+++ RestInPractice/Assets/Scripts/GameManager.cs
@@ -1,4 +1,27 @@
-﻿public class GameManager
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour 
 {
-    
+	public List<GameObject> minions;
+	protected int health;
+    protected int atkDmg;
+	static gameManager instance;
+
+      public static gameManager GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new gameManager();
+        }
+        return instance;
+    }	
+	
+	void Start()
+	{
+		instance = this;
+		
+	}
+	
 }
